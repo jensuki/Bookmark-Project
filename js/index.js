d850659310df96c5d5a013c07da0d3615aaab52e
jensuki <jeni.suk@proton.me>
2025-04-11 14:52:06 -0700

    add functionality for displaying panels based on data-targets

---
+++ js/index.js
@@ -0,0 +1,33 @@
+// grab all tabs and panels
+const tabs = document.querySelectorAll('.tab');
+const panels = document.querySelectorAll('.panel');
+
+// tabs menu event listener
+tabs.forEach((tab) => tab.addEventListener('click', onTabClick));
+
+function onTabClick(e) {
+
+    // deactivate all tabs
+    tabs.forEach((tab) => {
+        tab.children[0].classList.remove(
+            'border-softRed',
+            'border-b-4',
+            'md:border-b-0'
+        )
+    })
+
+    // hide all panels when tab clicked
+    panels.forEach((panel) => panel.classList.add('hidden'));
+
+    // activate a new tab
+    e.target.classList.add('border-softRed', 'border-b-4');
+
+    // activate correct panel based on data-target
+    const classString = e.target.getAttribute('data-target');
+
+    // from section with id 'panels'
+    // show panel that matches the clicked tabs data-target
+    document.getElementById('panels')
+        .getElementsByClassName(classString)[0]
+        .classList.remove('hidden');
+}
